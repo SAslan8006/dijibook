ab5e4bd01ef4210beb4b5f7f80abccb8b2e7bdcf
Semih Turan <semituran@gmail.com>
2022-12-09 14:32:15 +0300

    style: eslint type fixed

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { FlatList, View, Text, ScrollView, TouchableWithoutFeedback } from 'react-native';
 import BookCard from '~/components/Card/BookCard';
@@ -98,33 +99,93 @@ const Home = ({ navigation }) => {
 
   return (
     <View style={styles.topContainer}>
-    <View style={styles.container}>
-      <View style={styles.category}>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          <TouchableWithoutFeedback onPress={handleSelectedAll}>
-            <Text style={[styles.categoryItem, {color: activeAll ? colors.orange : 'black', borderColor: activeAll ? colors.orange : 'black', backgroundColor: activeAll ? colors.white : 'white' } ]}>All</Text>
-          </TouchableWithoutFeedback>
-          <TouchableWithoutFeedback onPress={handleSelectedRoman}>
-            <Text style={[styles.categoryItem, {color: activeRoman ? colors.orange : 'black', borderColor: activeRoman ? colors.orange : 'black', backgroundColor: activeRoman ? colors.white : 'white' } ]}>Roman</Text>
-          </TouchableWithoutFeedback>
-          <TouchableWithoutFeedback onPress={handleSelectedSiir}>
-            <Text style={[styles.categoryItem, {color: activeSiir ? colors.orange : 'black', borderColor: activeSiir ? colors.orange : 'black', backgroundColor: activeSiir ? colors.white : 'white' } ]}>Siir</Text>
-          </TouchableWithoutFeedback>
-          <TouchableWithoutFeedback onPress={handleSelectedDeneme}>
-            <Text style={[styles.categoryItem, {color: activeDeneme ? colors.orange : 'black', borderColor: activeDeneme ? colors.orange : 'black', backgroundColor: activeDeneme ? colors.white : 'white' } ]}>Deneme</Text>
-          </TouchableWithoutFeedback>
-          <TouchableWithoutFeedback onPress={handleSelectedBiyografi}>
-            <Text style={[styles.categoryItem, {color: activeBiyografi ? colors.orange : 'black', borderColor: activeBiyografi ? colors.orange : 'black', backgroundColor: activeBiyografi ? colors.white : 'white' } ]}>Biyografi</Text>
-          </TouchableWithoutFeedback>
-          <TouchableWithoutFeedback onPress={handleSelectedEdebiyatInceleme}>
-            <Text style={[styles.categoryItem, {color: activeEdebiyat ? colors.orange : 'black', borderColor: activeEdebiyat ? colors.orange : 'black', backgroundColor: activeEdebiyat ? colors.white : 'white' } ]}>Edebiyat Inceleme</Text>
-          </TouchableWithoutFeedback>
-        </ScrollView>
+      <View style={styles.container}>
+        <View style={styles.category}>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            <TouchableWithoutFeedback onPress={handleSelectedAll}>
+              <Text
+                style={[
+                  styles.categoryItem,
+                  {
+                    color: activeAll ? colors.orange : 'black',
+                    borderColor: activeAll ? colors.orange : 'black',
+                    backgroundColor: activeAll ? colors.white : 'white',
+                  },
+                ]}>
+                All
+              </Text>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={handleSelectedRoman}>
+              <Text
+                style={[
+                  styles.categoryItem,
+                  {
+                    color: activeRoman ? colors.orange : 'black',
+                    borderColor: activeRoman ? colors.orange : 'black',
+                    backgroundColor: activeRoman ? colors.white : 'white',
+                  },
+                ]}>
+                Roman
+              </Text>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={handleSelectedSiir}>
+              <Text
+                style={[
+                  styles.categoryItem,
+                  {
+                    color: activeSiir ? colors.orange : 'black',
+                    borderColor: activeSiir ? colors.orange : 'black',
+                    backgroundColor: activeSiir ? colors.white : 'white',
+                  },
+                ]}>
+                Siir
+              </Text>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={handleSelectedDeneme}>
+              <Text
+                style={[
+                  styles.categoryItem,
+                  {
+                    color: activeDeneme ? colors.orange : 'black',
+                    borderColor: activeDeneme ? colors.orange : 'black',
+                    backgroundColor: activeDeneme ? colors.white : 'white',
+                  },
+                ]}>
+                Deneme
+              </Text>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={handleSelectedBiyografi}>
+              <Text
+                style={[
+                  styles.categoryItem,
+                  {
+                    color: activeBiyografi ? colors.orange : 'black',
+                    borderColor: activeBiyografi ? colors.orange : 'black',
+                    backgroundColor: activeBiyografi ? colors.white : 'white',
+                  },
+                ]}>
+                Biyografi
+              </Text>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={handleSelectedEdebiyatInceleme}>
+              <Text
+                style={[
+                  styles.categoryItem,
+                  {
+                    color: activeEdebiyat ? colors.orange : 'black',
+                    borderColor: activeEdebiyat ? colors.orange : 'black',
+                    backgroundColor: activeEdebiyat ? colors.white : 'white',
+                  },
+                ]}>
+                Edebiyat Inceleme
+              </Text>
+            </TouchableWithoutFeedback>
+          </ScrollView>
+        </View>
+        <View style={styles.flatlist}>
+          <FlatList data={contentList} renderItem={renderContent} numColumns={2} />
+        </View>
       </View>
-      <View style={styles.flatlist}>
-        <FlatList data={contentList} renderItem={renderContent} numColumns={2} />
-      </View>
-    </View>
     </View>
   );
 };
